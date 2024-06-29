885f003bf3117d31a06347aab8f350f166a37c07
schmev <86550672+schmev91@users.noreply.github.com>
2024-06-29 17:07:52 +0700

    hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,25 @@ import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    // darkMode: "selector",
-    content: [
-        "**/*.{js,html}",
-    ],
+  // darkMode: "selector",
+  content: ["index.html"],
 
-    theme: {
-        extend: {
-            colors: {
-                main: "#432FBB",
-            },
-            container: {
-                padding: {
-                    DEFAULT: "1rem",
-                    sm: "2rem",
-                    lg: "4rem",
-                    xl: "5rem",
-                    "2xl": "8rem",
-                },
-            },
+  theme: {
+    extend: {
+      colors: {
+        main: "#432FBB",
+      },
+      container: {
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "3rem",
         },
+      },
     },
+  },
 
-    plugins: [
-        require('daisyui'),
-    ],
+  plugins: [require("daisyui")],
 };
